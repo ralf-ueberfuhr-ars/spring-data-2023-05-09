42a4f735c7e70fc154ac519ba908445e70f2c1de
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2023-05-10 06:36:55 +0200

    Temporarily created model

--- src/main/java/de/sample/spring/customers/CustomerController.java
+++ src/main/java/de/sample/spring/customers/CustomerController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,9 +35,22 @@ public class CustomerController {
     @GetMapping(
       produces = MediaType.APPLICATION_JSON_VALUE
     )
-    public Collection<Customer> findAllCustomers() {
-        return repo.findAll();
+    public Collection<Customer> findAllCustomers(
+      @RequestParam(value = "interest", required = false) String interest
+    ) {
+        return (null != interest)
+          ? repo.findAllByInterestsContainingIgnoreCase(interest)
+          : repo.findAll();
     }
+
+    @GetMapping(
+      value = "/active",
+      produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Collection<Customer> findActiveCustomers() {
+        return this.repo.findAllActive();
+    }
+
 
     @GetMapping(
       value = "/{id}",
--- src/main/java/de/sample/spring/customers/CustomerRepository.java
+++ src/main/java/de/sample/spring/customers/CustomerRepository.java
@@ -1,10 +1,17 @@
 package de.sample.spring.customers;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, UUID> {
+
+    List<Customer> findAllByInterestsContainingIgnoreCase(String interest);
+
+    @Query("SELECT c FROM Customer c WHERE c.state = de.sample.spring.customers.CustomerState.ACTIVE")
+    List<Customer> findAllActive();
 }
